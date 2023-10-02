3e30fdc4909556c0732f75b2c12ff3eee42056aa
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2023-10-02 13:57:32 +0200

    Initial commit

---
+++ V8-R8-Hub/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace V8_R8_Hub.Controllers {
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class GameController : ControllerBase {
+        [HttpGet("benis", Name = "HelloWorld")]
+        public IActionResult HelloWorld() {
+            return Ok("Hello World!");
+        }
+    }
+}
