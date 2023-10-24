0713b8961332cc81e79a1fbc792db4821feb747e
Thomas Larsen <74708296+TBLarsen@users.noreply.github.com>
2023-10-24 12:55:04 +0200

    works in vr
    - controls for vr
    - options menu (unfinished)

--- Frontal-ende/menu.js
+++ Frontal-ende/menu.js
@@ -1,6 +1,21 @@
 var Ascene = document.querySelector('a-scene');
-var menu = document.createElement('a-rounded');
 
+var rightHand = document.createElement('a-entity');
+rightHand.setAttribute('id', 'rightHand');
+rightHand.setAttribute('mixin', 'hand');
+rightHand.setAttribute('oculus-touch-controls', { hand: 'right' });
+rightHand.setAttribute('laser-controls', '');
+
+var leftHand = document.createElement('a-entity');
+leftHand.setAttribute('id', 'leftHand');
+leftHand.setAttribute('mixin', 'hand');
+leftHand.setAttribute('oculus-touch-controls', { hand: 'left' });
+
+var cameraRig = document.getElementById('cameraRig');
+cameraRig.appendChild(rightHand);
+cameraRig.appendChild(leftHand);
+
+var menu = document.createElement('a-rounded');
 Ascene.appendChild(menu);
 menu.setAttribute("position", { x: -1.5, y: 0.5, z: -3 });
 menu.setAttribute('width', 3);
@@ -10,7 +25,7 @@ menu.setAttribute('height', 4)
 function AddMenuItemsToMenu(menu) {
     var arr = ['Menu', 'Games', 'Options', 'Quit'];
     var y = 3;
-    for (let i = 0; i <= arr.length-1; i++) {
+    for (let i = 0; i <= arr.length - 1; i++) {
         var menuitem = document.createElement('a-rounded')
         menu.appendChild(menuitem);
         menuitem.setAttribute('width', 2.5);
@@ -19,20 +34,19 @@ function AddMenuItemsToMenu(menu) {
         y -= 0.9
         if (arr[i] === 'Menu') {
             menuitem.setAttribute('color', 'black');
-            AddTextMenuitem(menuitem, arr[i], '#FFF');
+            AddText(menuitem, arr[i], '#FFF');
         }
-        else{
+        else {
             menuitem.setAttribute('color', 'grey');
-            AddTextMenuitem(menuitem, arr[i], 'black');
+            AddText(menuitem, arr[i], 'black');
         }
         menuitem.setAttribute('id', arr[i]);
-
-        console.log(menuitem);
     }
 }
-function AddTextMenuitem(menuitem, string, color) {
+
+function AddText(item, string, color) {
     var text = document.createElement('a-text')
-    menuitem.appendChild(text)
+    item.appendChild(text)
     text.setAttribute("position", { x: 0.05, y: 0.4, z: 0.01 });
     text.setAttribute("scale", { x: 3, y: 3, z: 3 });
     text.setAttribute("value", string);
@@ -41,7 +55,35 @@ function AddTextMenuitem(menuitem, string, color) {
 
 AddMenuItemsToMenu(menu);
 
-document.querySelector('#Quit').addEventListener('mousedown', function (evt){
-    menu.setAttribute('animation', {property: 'position', to: {x: -1.5, y: -10, z: -3}, dur: 2000, loop: 'false'})
-    console.log("sus")
-})
+// document.querySelector('#Quit').addEventListener('mousedown', function (evt){
+//     menu.setAttribute('animation', {property: 'position', to: {x: -1.5, y: -10, z: -3}, dur: 2000, loop: 'false'})
+// });
+
+// document.querySelector('#Games').addEventListener('mousedown', function(evt){
+//     menu.setAttribute('animation', {property: 'position', to: {x: -20, y: 0.5, z: -3}, dur: 2000, loop: 'false'})
+// })  
+
+document.querySelector('#Options').addEventListener('mousedown', function (evt) {
+    ClearMenu();
+    var optionitem = document.createElement('a-rounded');
+    menu.appendChild(optionitem);
+    optionitem.setAttribute('color', 'black');
+    AddText(optionitem, 'Options', '#FFF');
+    optionitem.setAttribute('width', 2.5);
+    optionitem.setAttribute('height', 0.8);
+    optionitem.setAttribute("position", { x: 0.25, y: 3, z: 0.01 });
+
+    var left = document.createElement('a-rounded');
+    left.setAttribute('width', 1)
+    left.setAttribute('height', 0.8);
+    left.setAttribute("position", { x: 0.25, y: 3, z: 0.01 });
+    
+
+
+});
+
+function ClearMenu() {
+    while (menu.firstChild) {
+        menu.removeChild(menu.lastChild)
+    }
+}
