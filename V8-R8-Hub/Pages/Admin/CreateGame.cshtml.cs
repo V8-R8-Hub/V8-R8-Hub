b78849386c9fd6d003ce6b12ba52bf29c22bc1b1
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2023-11-08 11:37:05 +0100

    added support for multiple file upload (#10)

--- V8-R8-Hub/Pages/Admin/CreateGame.cshtml.cs
+++ V8-R8-Hub/Pages/Admin/CreateGame.cshtml.cs
@@ -7,14 +7,18 @@ namespace V8_R8_Hub.Pages.Admin
 	public class CreateGameModel : PageModel
 	{
 		private readonly IGameService _gameService;
+		public string AllowedGameMimeTypesString { get; set; }
+		public string AllowedThumbnailMimeTypesString { get; set; }
 
 		public CreateGameModel(IGameService gameService) {
 			_gameService = gameService;
 		}
 
 		public async Task OnGetAsync() {
-			ViewData["AllowedGameMimeTypesString"] = string.Join(", ", await _gameService.GetAllowedGameMimeTypes());
-			ViewData["AllowedThumbnailMimeTypesString"] = string.Join(", ", await _gameService.GetAllowedThumbnailMimeTypes());
+			await Task.WhenAll(
+				Task.Run(async () => AllowedGameMimeTypesString = string.Join(", ", await _gameService.GetAllowedGameMimeTypes())),
+				Task.Run(async () => AllowedThumbnailMimeTypesString = string.Join(", ", await _gameService.GetAllowedThumbnailMimeTypes()))
+			);
 		}
 	}
 }
